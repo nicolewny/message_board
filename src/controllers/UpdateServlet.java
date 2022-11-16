15754c42d75a9820c7b84d0665dcb99948d390ae
ngayee.wong <ngayee.wong@growship.com>
2022-11-16 17:19:28 +0900

    更新

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -78,5 +78,6 @@ public class UpdateServlet extends HttpServlet {
                 // indexページへリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
+        }
     }
 }
